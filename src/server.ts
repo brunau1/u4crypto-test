55f88631322960d372c075861b2f843d7ad6d06c
Bruno Santos <brunosantosprotec@gmail.com>
2021-06-16 00:07:23 -0300

    name "handlers" to "controllers"

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as Hapi from '@hapi/hapi';
 import JWT from './utils/jwt';
+import * as DB from './database/postgre';
 
 const server = new Hapi.Server({ port: 3000 });
 JWT.register(server);
